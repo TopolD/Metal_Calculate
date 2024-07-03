a2df1351e8db8c6a4c11b127a261d3764bd751e9
TopolD <133087769+TopolD@users.noreply.github.com>
2024-07-04 00:16:59 +0300

    add class Absortion....

--- utils_db/Inser_Delete_Update_Db.py
+++ utils_db/Inser_Delete_Update_Db.py
@@ -1,37 +1,35 @@
-from utils_db.services import CreateServices, DeleteServices, UpdateServices
+from utils_db.services import AbsorptionRateEntity
 
 
 class WorkWithEntityInstances:
 
-    def __init__(self, NameDb):
-        self.csv = CreateServices()
-        self.dsv = DeleteServices()
-        self.usv = UpdateServices()
+    def __init__(self):
+        self.Absorp = AbsorptionRateEntity()
 
     def MathchData(self, Data: dict):
         try:
             match Data.get("NameDb"):
                 case 'AbsorptionRate':
-                    return self.csv.CreateAbsorptionRateEntity(Data)
+                    return self.Absorp()
                 case 'ChemicalComposition':
-                    return self.csv.CreateChemicalCompositionEntity(Data)
+                    return self.Se.CreateChemicalCompositionEntity(Data)
                 case 'CoredWire':
-                    return self.csv.CreateCoredWireEntity(Data)
+                    return self.Se.CreateCoredWireEntity(Data)
                 case 'Fuse':
-                    return self.csv.CreateFuseEntity(Data)
+                    return self.Se.CreateFuseEntity(Data)
                 case _:
                     return False
         except Exception as e:
             print(f'Was  caused an exception {e}')
 
-    def CreatingEntity(self, DictData: dict):
+    def CreatingEntity(self):
         try:
-            self.MathchData(DictData)
+            self.MathchData()
         except Exception as e:
             print(f' Was cause Exseption {e}')
 
-    def DeleteEntity(self, NameCol):
+    def DeleteEntity(self):
         pass
 
-    def UpdateEntity(self, NameCol, Col, Data: dict):
-        pass
+    def UpdateEntity(self):
+        try:
--- utils_db/services.py
+++ utils_db/services.py
@@ -2,13 +2,9 @@ from pony.orm import db_session
 from utils_db.Db_models import ConnDb
 
 
-class CreateServices:
+class AbsorptionRateEntity(ConnDb):
 
-    def __init__(self):
-        self.db = ConnDb()
-
-    def CreateAbsorptionRateEntity(self, Data: dict):
-
+    def CreateAbsorptionRateEntity(self,Data ):
         with db_session:
             try:
                 NewAbsorptionRate = self.db.AbsorptionRate(
@@ -33,76 +29,118 @@ class CreateServices:
                 print(f' Created AbsorptionRate {NewAbsorptionRate}')
             except Exception as e:
                 print(f' Was cause Exseption {e}')
+    def DeleteEntityAbsor(self):
 
-    def CreateChemicalCompositionEntity(self, Data: dict):
-        with db_session:
-            try:
-                NewChemicalComposition = self.db.ChemicalComposition(
-                    MaterialName=Data.get('MaterialName'),
-                    C=Data.get('C'),
-                    Mn=Data.get('Mn'),
-                    Si=Data.get('Si'),
-                    Cr=Data.get('Cr'),
-                    Ti=Data.get('Ti'),
-                    V=Data.get('V'),
-                    Mo=Data.get('Mo'),
-                    B=Data.get('B'),
-                    Nb=Data.get('Nb'),
-                    Ni=Data.get('Ni'),
-                    Cu=Data.get('Cu'),
-                    Al=Data.get('Al'),
-                    S=Data.get('S'),
-                    Fe=Data.get('Fe'),
-                    Ca=Data.get('Ca'),
-                    P=Data.get('P'),
-                )
-                print(f' Created ChemicalComposition {NewChemicalComposition}')
-            except Exception as e:
-                print(f' Was cause Exseption {e}')
+    def UpdateEntityAbsor(self):
 
-    def CreateFuseEntity(self, Data: dict):
-        with db_session:
-            try:
-                NewFuse = self.db.Fuse(
-                    Tcn=Data.get('Tcn'),
-                    FuseName=Data.get('FuseName'),
-                    TempVd=Data.get('TempVd'),
-                    Temp_ccm1=Data.get('Temp_ccm1'),
-                    Temp_ccm2=Data.get('Temp_ccm2'),
-                    C=Data.get('C'),
-                    Si=Data.get('Si'),
-                    Mn=Data.get('Mn'),
-                    S=Data.get('S'),
-                    Al=Data.get('Al'),
-                    Cr=Data.get('Cr'),
-                    Mo=Data.get('Mo'),
-                    Ni=Data.get('Ni'),
-                    Cu=Data.get('Cu'),
-                    V=Data.get('V'),
-                    Nb=Data.get('Nb'),
-                    Ti=Data.get('Ti'),
-                    B=Data.get('B'),
-                    Ca=Data.get('Ca'),
-                    Cpr=Data.get('Cpr'),
-                )
 
-                print(f' Created Fuse {NewFuse}')
-            except Exception as e:
-                print(f' Was cause Exseption {e}')
+class ChemicalCompositionEntity
 
-    def CreateCoredWireEntity(self, Data: dict):
-        with db_session:
-            try:
-                NewCoreWire = self.db.CoredWire(
-                    CWName=Data.get('CWName'),
-                    provider=Data.get('provider'),
-                    CorWire=Data.get('CorWire'),
-                    weight=Data.get('weight')
-                )
 
-                print(f' Created CoreWire {NewCoreWire}')
-            except Exception as e:
-                print(f' Was cause Exseption {e}')
+class CoredWireEntity
+
+
+class FuseEntity
+
+
+def CreateAbsorptionRateEntity(self, Data: dict):
+    with db_session:
+        try:
+            NewAbsorptionRate = self.db.AbsorptionRate(
+                AbrName=Data.get('AbrName'),
+                C=Data.get('C'),
+                Mn=Data.get('Mn'),
+                Si=Data.get('Si'),
+                Cr=Data.get('Cr'),
+                Ti=Data.get('Ti'),
+                V=Data.get('V'),
+                Mo=Data.get('Mo'),
+                B=Data.get('B'),
+                Nb=Data.get('Nb'),
+                Ni=Data.get('Ni'),
+                Cu=Data.get('Cu'),
+                Al=Data.get('Al'),
+                S=Data.get('S'),
+                Fe=Data.get('Fe'),
+                Ca=Data.get('Ca'),
+                P=Data.get('P'),
+            )
+            print(f' Created AbsorptionRate {NewAbsorptionRate}')
+        except Exception as e:
+            print(f' Was cause Exseption {e}')
+
+
+def CreateChemicalCompositionEntity(self, Data: dict):
+    with db_session:
+        try:
+            NewChemicalComposition = self.db.ChemicalComposition(
+                MaterialName=Data.get('MaterialName'),
+                C=Data.get('C'),
+                Mn=Data.get('Mn'),
+                Si=Data.get('Si'),
+                Cr=Data.get('Cr'),
+                Ti=Data.get('Ti'),
+                V=Data.get('V'),
+                Mo=Data.get('Mo'),
+                B=Data.get('B'),
+                Nb=Data.get('Nb'),
+                Ni=Data.get('Ni'),
+                Cu=Data.get('Cu'),
+                Al=Data.get('Al'),
+                S=Data.get('S'),
+                Fe=Data.get('Fe'),
+                Ca=Data.get('Ca'),
+                P=Data.get('P'),
+            )
+            print(f' Created ChemicalComposition {NewChemicalComposition}')
+        except Exception as e:
+            print(f' Was cause Exseption {e}')
+
+
+def CreateFuseEntity(self, Data: dict):
+    with db_session:
+        try:
+            NewFuse = self.db.Fuse(
+                Tcn=Data.get('Tcn'),
+                FuseName=Data.get('FuseName'),
+                TempVd=Data.get('TempVd'),
+                Temp_ccm1=Data.get('Temp_ccm1'),
+                Temp_ccm2=Data.get('Temp_ccm2'),
+                C=Data.get('C'),
+                Si=Data.get('Si'),
+                Mn=Data.get('Mn'),
+                S=Data.get('S'),
+                Al=Data.get('Al'),
+                Cr=Data.get('Cr'),
+                Mo=Data.get('Mo'),
+                Ni=Data.get('Ni'),
+                Cu=Data.get('Cu'),
+                V=Data.get('V'),
+                Nb=Data.get('Nb'),
+                Ti=Data.get('Ti'),
+                B=Data.get('B'),
+                Ca=Data.get('Ca'),
+                Cpr=Data.get('Cpr'),
+            )
+
+            print(f' Created Fuse {NewFuse}')
+        except Exception as e:
+            print(f' Was cause Exseption {e}')
+
+
+def CreateCoredWireEntity(self, Data: dict):
+    with db_session:
+        try:
+            NewCoreWire = self.db.CoredWire(
+                CWName=Data.get('CWName'),
+                provider=Data.get('provider'),
+                CorWire=Data.get('CorWire'),
+                weight=Data.get('weight')
+            )
+
+            print(f' Created CoreWire {NewCoreWire}')
+        except Exception as e:
+            print(f' Was cause Exseption {e}')
 
 
 class DeleteServices(CreateServices):
@@ -117,14 +155,11 @@ class UpdateServices(CreateServices):
     def UpdateService(self):
         with db_session:
 
-
             try:
 
-                NameDbList = ['AbsorptionRate', 'ChemicalComposition', 'CoredWire', 'Fuse'].pop(self.Data.get('NameDb'))
+                NewRequest = 0
 
-                NewRequest = self.db.NameDbList.get(self.Data.get('Tcn'))
 
-                NewRequest[NewRequest.ID] = set(**self.Data)
 
             except Exception as e:
                 pass
