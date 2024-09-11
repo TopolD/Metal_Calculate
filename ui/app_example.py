db728a21f511c383fb8b55784f5c095151090ada
TopolD <133087769+TopolD@users.noreply.github.com>
2024-09-11 21:08:39 +0300

    add mechanic for window

--- ui/app_example.py
+++ ui/app_example.py
@@ -1,18 +1,18 @@
-from idlelib.autocomplete import TRY_A
-
-from PyQt5.QtCore import QTimer
-from PyQt5.QtWidgets import QMainWindow, QLabel
+from PyQt5.QtWidgets import QMainWindow, QWidget
 from PyQt5.QtGui import QDoubleValidator, QIntValidator
-from Gui import *
+from Lrf1 import *
 from utils_calculate.Formuls_example import *
+from Gui import *
 
 
-class DisplayWindow(QMainWindow, Ui_MainWindow):
+class DisplayWindow(QWidget, Ui_LRF1_Widget):
     def __init__(self):
         super(DisplayWindow, self).__init__()
+
         self.setupUi(self)
         self.DisplayTablesForInputData()
         self.get_weight_and_tech_card()
+
 
     def DisplayTablesForInputData(self):
         self.DisplayExternalElementMaterials()
@@ -42,6 +42,21 @@ class DisplayWindow(QMainWindow, Ui_MainWindow):
         self.label_246.setText(value.get('TempVd'))
         self.label_248.setText(value.get('Temp_ccm1'))
         self.label_249.setText(value.get('Temp_ccm2'))
+        self.label_14.setText(str(value.get('C', 'N/A')))
+        self.label_15.setText(str(value.get('Si', 'N/A')))
+        self.label_16.setText(str(value.get('Mn', 'N/A')))
+        self.label_17.setText(str(value.get('Cr', 'N/A')))
+        self.label_18.setText(str(value.get('Ni', 'N/A')))
+        self.label_19.setText(str(value.get('Cu', 'N/A')))
+        self.label_20.setText(str(value.get('Mo', 'N/A')))
+        self.label_21.setText(str(value.get('V', 'N/A')))
+        self.label_22.setText(str(value.get('Nb', 'N/A')))
+        self.label_23.setText(str(value.get('B', 'N/A')))
+        self.label_24.setText('-')
+
+        self.label_45.setText(str(value.get('Cpr', 'N/A')))
+        self.label_48.setText(str(value.get('Al', 'N/A')))
+        self.label_46.setText(str(value.get('Ti', 'N/A')))
 
     def get_weight_and_tech_card(self):
         self.lineEdit_67.setValidator(QIntValidator(0, 999))
@@ -84,13 +99,6 @@ class DisplayWindow(QMainWindow, Ui_MainWindow):
             if widget is not None:
                 widget.setVisible(visible)
 
-    @staticmethod
-    def verification_conditions(operator, value):
-        if operator:
-            value.setVisible(True)
-        else:
-            value.setVisible(False)
-
     def update_label(self):
         DataHolder.set_data(self.lineEdit_67.text(), None)
         FuseData = GetDataCalculate()
@@ -107,10 +115,101 @@ class DisplayWindow(QMainWindow, Ui_MainWindow):
             constant.setStyleSheet("border-bottom: 2px solid #34C759;")
 
 
+# переделать под чистый код
+class Window1(DisplayWindow):
+    def __init__(self, main_window):
+        super(Window1, self).__init__()
+        self.setWindowTitle('Calculate - Lrf1')
+        self.main_window = main_window
+        self.pushButton_2.clicked.connect(self.switch_to_window_2)
+
+    def switch_to_window_2(self):
+        self.hide()
+        self.main_window.show_window_2()
+
+
+class Window2(DisplayWindow):
+    def __init__(self, main_window):
+        super(Window2, self).__init__()
+        self.setWindowTitle('Calculate - Lrf2')
+        self.main_window = main_window
+        self.pushButton.clicked.connect(self.switch_to_window_1)
+
+    def switch_to_window_1(self):
+        self.hide()
+        self.main_window.show_window_1()
+
+
+class Window3(DisplayWindow):
+    def __init__(self, main_window):
+        super(Window3, self).__init__()
+        self.setWindowTitle('Calculate - Разбавление')
+        self.main_window = main_window
+        self.pushButton_3.clicked.connect(self.switch_to_window_3)
+
+    def switch_to_window_3(self):
+        self.hide()
+        self.main_window.show_window_3()
+
+
+class Window4(DisplayWindow):
+    def __init__(self, main_window):
+        super(Window4, self).__init__()
+        self.setWindowTitle('Calculate - Формулы')
+        self.main_window = main_window
+        self.pushButton_4.clicked.connect(self.switch_to_window_4)
+
+    def switch_to_window_4(self):
+        self.hide()
+        self.main_window.show_window_4()
+
+
+class Window5(DisplayWindow):
+    def __init__(self, main_window):
+        super(Window5, self).__init__()
+        self.setWindowTitle('Calculate - Заметки')
+        self.main_window = main_window
+        self.pushButton_5.clicked.connect(self.switch_to_window_5)
+
+    def switch_to_window_5(self):
+        self.hide()
+        self.main_window.show_window_5()
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setWindowTitle('Main Window')
+
+        self.lrf1 = Window1(self)
+        self.lrf2 = Window2(self)
+        self.lrf3 = Window3(self)
+        self.lrf4 = Window4(self)
+        self.lrf5 = Window5(self)
+
+    def show_window_1(self):
+        self.lrf1.show()
+
+    def show_window_2(self):
+        self.lrf2.show()
+
+    def show_window_3(self):
+        self.lrf3.show()
+
+    def show_window_4(self):
+        self.lrf4.show()
+
+    def show_window_5(self):
+        self.lrf5.show()
+
+
 if __name__ == "__main__":
     import sys
 
     app = QtWidgets.QApplication(sys.argv)
-    MainWindow = DisplayWindow()
-    MainWindow.show()
+
+    main_window = MainWindow()
+
+    main_window.show_window_1()
+
     sys.exit(app.exec_())
